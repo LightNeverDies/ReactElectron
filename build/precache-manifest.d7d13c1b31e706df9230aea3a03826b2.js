4f9771ad9e0e0df2e6002106be6b0405432f5e2f
Georgi Georgiev <ggeorgiev@hilscherdtc.local>
2020-07-01 10:51:10 +0300

    Electron Setup

---
+++ build/precache-manifest.d7d13c1b31e706df9230aea3a03826b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc3a45f830e0b2f3cf4b4c72d2add3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b52f3fce7c92d0da243",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a013016a01a9923551a8",
+    "url": "./static/js/2.cddf1824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cddf1824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b52f3fce7c92d0da243",
+    "url": "./static/js/main.023d07b0.chunk.js"
+  },
+  {
+    "revision": "be3bfa269221ae78d40f",
+    "url": "./static/js/runtime-main.2748b377.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends React. Component{
     render(){
         return(
             <div>
-            <h1>Hello Team</h1>
+            <h1>Hello World</h1>
             </div>
         )
     }
